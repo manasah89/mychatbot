fd4bba3f7413c066cea9a57f93b3e447f83cb81b
manasah89 <manasa.h@isteer.com>
2024-07-24 15:15:13 +0530

    added all files

---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+from constant import openai_key  # Make sure this module provides your OpenAI API key
+from langchain_google_genai import GoogleGenerativeAI
+import streamlit as st
+
+# Ensure the OpenAI API key is set correctly
+os.environ['GOOGLE_API_KEY'] = "AIzaSyAIr7m1lYDleEVKdCnSXAZrWAD15MuhKVw"  # Ensure `openai_key` is the correct OpenAI key
+
+# Streamlit framework
+st.title("Langchain demo with OpenAI API")
+input_text = st.text_input("Search the topic you want")
+
+# OpenAI LLMs
+llm = GoogleGenerativeAI(model="gemini-pro",temperature=0.8)
+
+if input_text:
+    st.write(llm(input_text))
